256e94c5da10b316420b7f9c980f00033bd5dbff
zedonora <timeundtime@gmail.com>
2020-05-26 05:39:50 +0900

    setting admin

--- polls/models.py
+++ polls/models.py
@@ -15,7 +15,11 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently'
+    # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
